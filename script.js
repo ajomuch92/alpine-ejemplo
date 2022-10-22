17b54914d0e4d537916fe8512eebd63f815f981f
ajomuch92 <aaronmontes_92@yahoo.es>
2022-10-21 18:05:00 -0600

    feat: 🎸 Agregando alpine al proyecto

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ function manejadorClick() {
   const mensaje = document.getElementById('mensaje-bienvenida');
   mensaje.innerText = 'Bienvenido ' + nombre;
 }
+
+function ocultarMostrar() {
+  const elemento = document.getElementById('contenido');
+  if (elemento.style.display === 'none') {
+    elemento.style.display = 'block';
+  } else {
+    elemento.style.display = 'none';
+  }
+}
+
+function initAlpine() {
+
+  const suma = function (a,b) {
+    return a + b;
+  }
+
+  return {
+    open: true,
+    nombre: '',
+    mostrarAlerta() {
+      alert('Bienvenido');
+    },
+    preguntarPorElNombre() {
+      this.nombre = prompt('¿Cuál es tu nombre?');
+    },
+    suma,
+  }
+}
